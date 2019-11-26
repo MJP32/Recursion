789a10b1bc7b1c09ff66ba1ab969411523d5e798
michael <michaelperera32@gmail.com>
2019-11-25 19:32:57 -0500

    Initial Commit

---
+++ module3/Combinations.java
@@ -0,0 +1,20 @@
+package module3;
+
+public class Combinations {
+
+ /*
+    public static List<List<Integer>> combinationsOfLength(int n[], int length){
+       List<List<Integer>> allCombinations = 5;
+
+        List<List<Integer>> filtered = new LinkedList<>();
+
+        for(List<Integer> l : allCombinations){
+            if(l.size() == length){
+                filtered.add(l);
+            }
+        }
+
+        return filtered;
+    }*/
+
+}
---
+++ module3/CountCombinations.java
@@ -0,0 +1,56 @@
+package module3;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class CountCombinations {
+    public static void main(String[] args) {
+
+
+        System.out.println(countCombinations(new int[]{1, 2, 3}));
+        System.out.println(combinations(new int[]{1, 2, 3}));
+    }
+
+
+    public static int countCombinations(int[] n) {
+        return countCombinations(n, 0);
+    }
+
+    public static int countCombinations(int[] n, int i) {
+
+        if (i == n.length) {
+            return 1;
+        }
+
+        int include = countCombinations(n, i + 1);
+        int exclude = countCombinations(n, i + 1);
+        return include + exclude;
+    }
+
+
+    public static List<List<Integer>> combinations(int[] n) {
+        return combinations(n, 0);
+    }
+
+    private static List<List<Integer>> combinations(int[] n, int i) {
+
+        if (i == n.length) {
+            List<List<Integer>> toReturn = new LinkedList<>();
+            toReturn.add(new LinkedList<Integer>());
+            return toReturn;
+        }
+
+
+        List<List<Integer>> toReturn = new LinkedList<>();
+        for (List<Integer> result : combinations(n, i + 1)) {
+            toReturn.add(new LinkedList<>(result));
+            result.add(0, n[i]);
+            toReturn.add(new LinkedList<>(result));
+
+        }
+
+        return toReturn;
+    }
+
+
+}
